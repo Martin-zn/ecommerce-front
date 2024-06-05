856aaf7b8f1df4cb1e0fb08b8ac518f658f31cbc
MartinZuniga <martin.zuniga@usach.cl>
2024-06-05 12:48:45 -0400

    Second part of front

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AddressCard = () => {
+    return (
+        <div>
+            <div className='space-y-3'>
+                <p className='font-semibold'>Santiago</p>
+                <p>El valle, 7049</p>
+                <div className='space-y-1'>
+                    <p className='font-semibold uppercase'>Numero de telefono</p>
+                    <p>+56 934418828</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AddressCard
---
+++ src/customer/components/Cart/Cart.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import CartItem from './CartItem'
+import { Button } from '@mui/material'
+
+const Cart = () => {
+  return (
+    <div>
+        <div className='lg:grid grid-cols-3 lg:px-16 lg:py-16 relative'>
+            <div className='col-span-2'>
+                {[1,1,1,1].map((item)=><CartItem/>)}
+                
+            </div>
+        <div className='px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0'>
+            <div className='border'>
+                <p className='uppercase font-bold opacity-60 pb-4'>price details</p>
+                <hr/>
+                <div className='space-y-3 font-semibold mb-10'>
+                    <div className='flex justify-between pt-3 text-black'>
+                        <span>Price</span>
+                        <span>$5.000</span>
+                    </div>
+                    <div className='flex justify-between pt-3 text-black'>
+                        <span>Price</span>
+                        <span>$5.000</span>
+                    </div>
+                    <div className='flex justify-between pt-3 text-black font-bold'>
+                        <span>Total</span>
+                        <span>$5.000</span>
+                    </div>
+
+                    
+
+                </div>
+                    <Button className=' w-full mt-5' variant="contained" sx={{px:"2.5rem",py:"1.5rem"}}>
+                        PAGAR
+                    </Button>
+            </div>
+
+        </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Cart
