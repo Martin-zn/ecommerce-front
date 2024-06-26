9f59d696fff496801143cfcf4d488fcf740b132b
MartinZuniga <martin.zuniga@usach.cl>
2024-06-26 14:07:14 -0400

    integracion front 26-6

--- src/customer/components/Navigation/Navigation.jsx
+++ src/customer/components/Navigation/Navigation.jsx
@@ -25,124 +25,110 @@ import { getUser, logout } from '../../../State/Auth/Action'
 
 const navigation = {
   categories: [
+    // {
+    //   id: 'women',
+    //   name: 'Women',
+    //   featured: [
+    //     {
+    //       name: 'New Arrivals',
+    //       href: '#',
+    //       imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+    //       imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+    //     },
+    //     {
+    //       name: 'Basic Tees',
+    //       href: '#',
+    //       imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+    //       imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+    //     },
+    //   ],
+    //   sections: [
+    //     {
+    //       id: 'clothing',
+    //       name: 'Clothing',
+    //       items: [
+    //         { name: 'Tops', href: '#' },
+    //         { name: 'Dresses', href: '#' },
+    //         { name: 'Pants', href: '#' },
+    //         { name: 'Denim', href: '#' },
+    //         { name: 'Sweaters', href: '#' },
+    //         { name: 'T-Shirts', href: '#' },
+    //         { name: 'Jackets', href: '#' },
+    //         { name: 'Activewear', href: '#' },
+    //         { name: 'Browse All', href: '#' },
+    //       ],
+    //     },
+    //     {
+    //       id: 'accessories',
+    //       name: 'Accessories',
+    //       items: [
+    //         { name: 'Watches', href: '#' },
+    //         { name: 'Wallets', href: '#' },
+    //         { name: 'Bags', href: '#' },
+    //         { name: 'Sunglasses', href: '#' },
+    //         { name: 'Hats', href: '#' },
+    //         { name: 'Belts', href: '#' },
+    //       ],
+    //     },
+    //     {
+    //       id: 'brands',
+    //       name: 'Brands',
+    //       items: [
+    //         { name: 'Full Nelson', href: '#' },
+    //         { name: 'My Way', href: '#' },
+    //         { name: 'Re-Arranged', href: '#' },
+    //         { name: 'Counterfeit', href: '#' },
+    //         { name: 'Significant Other', href: '#' },
+    //       ],
+    //     },
+    //   ],
+    // },
     {
-      id: 'women',
-      name: 'Women',
+      id: 'eventos',
+      name: 'Eventos',
       featured: [
         {
-          name: 'New Arrivals',
+          name: 'Conferencias',
           href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-          imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+          imageSrc: 'https://media.istockphoto.com/id/499517325/es/foto/un-hombre-hablando-en-una-conferencia-de-negocios.jpg?s=1024x1024&w=is&k=20&c=Czg5MsXPdya5hlFMAriNXAb0xZw7TPSXYelgCJ6pbz8=',
+          imageAlt: 'Ejemplo 1.',
         },
         {
-          name: 'Basic Tees',
+          name: 'Fiestas',
           href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        },
-      ],
-      sections: [
-        {
-          id: 'clothing',
-          name: 'Clothing',
-          items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Dresses', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Denim', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
-          ],
-        },
-        {
-          id: 'accessories',
-          name: 'Accessories',
-          items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Significant Other', href: '#' },
-          ],
-        },
-      ],
-    },
-    {
-      id: 'men',
-      name: 'Men',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-          imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-        },
-        {
-          name: 'Artwork Tees',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
+          imageSrc: 'https://media.istockphoto.com/id/486420378/es/foto/cabezal-de-nataci%C3%B3n-en-pista-de-baile.jpg?s=1024x1024&w=is&k=20&c=EegKjHBotOW7p8QxjHd7zl3FtPCBbZAhw2KUFUYS4F4=',
           imageAlt:
-            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+            'Ejemplo 2',
         },
       ],
       sections: [
         {
-          id: 'clothing',
-          name: 'Clothing',
+          id: 'conferencias',
+          name: 'Conferencias',
           items: [
-            { name: 'Tops', href: '#' },
-            { name: 'Pants', href: '#' },
-            { name: 'Sweaters', href: '#' },
-            { name: 'T-Shirts', href: '#' },
-            { name: 'Jackets', href: '#' },
-            { name: 'Activewear', href: '#' },
-            { name: 'Browse All', href: '#' },
+            { name: 'Conferencias 1', href: '#' },
+            { name: 'Conferencias 2', href: '#' },
+            { name: 'Conferencias 3', href: '#' },
+
           ],
         },
         {
-          id: 'accessories',
-          name: 'Accessories',
+          id: 'fiestas',
+          name: 'Fiestas',
           items: [
-            { name: 'Watches', href: '#' },
-            { name: 'Wallets', href: '#' },
-            { name: 'Bags', href: '#' },
-            { name: 'Sunglasses', href: '#' },
-            { name: 'Hats', href: '#' },
-            { name: 'Belts', href: '#' },
+            { name: 'Fiesta 1', href: '#' },
+            { name: 'Fiesta 1', href: '#' },
+            { name: 'Fiesta 1', href: '#' },
+   
           ],
         },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Re-Arranged', href: '#' },
-            { name: 'Counterfeit', href: '#' },
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
-          ],
-        },
+        ,
       ],
     },
   ],
   pages: [
-    { name: 'Company', href: '#' },
-    { name: 'Stores', href: '#' },
+    { name: 'Nosotros', href: '#' },
+    // { name: 'Stores', href: '#' },
   ],
 }
 
@@ -353,7 +339,7 @@ export default function Navigation() {
 
       <header className="relative bg-white">
         <p className="flex h-10 items-center justify-center bg-black px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-          Aqui va un descuento
+          Encuentra lo que te interesa
         </p>
 
         <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -371,8 +357,8 @@ export default function Navigation() {
 
               {/* Logo */}
               <div className="ml-4 flex lg:ml-0">
-                <a href="#">
-                  <span className="sr-only">Your Company</span>
+                <a href="localhost:3000/">
+                  <span className="sr-only">Nosotros</span>
                   <img
                     className="h-8 w-auto"
                     src="https://img.freepik.com/vector-gratis/fondo-plano-dia-san-valentin_52683-157845.jpg?t=st=1717165510~exp=1717169110~hmac=053ae258e1b8602d32f8f5cf64b66085e9d0a0881f2942551669af8f692cafb1&w=996"
@@ -431,7 +417,7 @@ export default function Navigation() {
                                             {item.name}
                                           </a>
                                           <p aria-hidden="true" className="mt-1">
-                                            Shop now
+                                            Comprar...
                                           </p>
                                         </div>
                                       ))}
